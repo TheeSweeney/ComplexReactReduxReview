51775735a70e62badfe9ea5b8d03d1e1a6ea9f3a
Brian Sweeney <brian.sweeney@cn-brian.sweeney-1740-mbpr>
2017-07-12 15:59:44 -0400

    created basic video list component and passed props into it from App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar'
-
+import VideoList from './components/video_list'
 import config from './config.js';
 
 const API_KEY = config.key;
@@ -26,6 +25,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
