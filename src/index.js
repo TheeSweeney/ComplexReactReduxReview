f5a9066882422565084adb1294dd2879b88aa055
Brian Sweeney <brian.sweeney@cn-brian.sweeney-1740-mbpr>
2017-07-12 11:36:22 -0400

    basic searchbar component created; api key imported from config file;`

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar'
+
 import config from './config.js';
 
 const API_KEY = config.key;
 
 //Create a new component - should produce some HTML
 const App = () => {
-	return <div> Hi! </div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 //Take component's generated HTML and put it on the page/in DOM
