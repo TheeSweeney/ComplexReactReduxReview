56cb66bdf0df14b2ce599ba6466675318989d085
Brian Sweeney <brian.sweeney@cn-brian.sweeney-1740-mbpr>
2017-07-13 10:43:01 -0400

    searchbar now searches for videos as you type

--- src/index.js
+++ src/index.js
@@ -17,8 +17,11 @@ class App extends Component {
 			selectedVideo: null,
 		}
 
+		this.videoSearch('complex media');
+	}
 
-		YTSearch({key: API_KEY, term: 'complex media'}, (videos) => {
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, (videos) => {
 			this.setState({ 
 				videos: videos,
 				selectedVideo: videos[0] })
@@ -28,7 +31,7 @@ class App extends Component {
 	render(){
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList 
 					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
